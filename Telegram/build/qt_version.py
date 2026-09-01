d282ae4291105d797119cbcfb1d3c074f130ecb6
Matzu <matzu@mod.local>
2026-09-01 21:14:55 +0100

    fix(build): use Qt 6 for Windows build to support modern kimageformats APIs

--- Telegram/build/qt_version.py
+++ Telegram/build/qt_version.py
@@ -4,10 +4,10 @@ def resolve(arch):
     if sys.platform == 'darwin':
         os.environ['QT'] = '6.11.1'
     elif sys.platform == 'win32':
-        if arch == 'arm' or 'qt6' in sys.argv:
-            print('Choosing Qt 6.')
-            os.environ['QT'] = '6.11.1'
-        else:
+        if 'qt5' in sys.argv:
             print('Choosing Qt 5.')
             os.environ['QT'] = '5.15.19'
+        else:
+            print('Choosing Qt 6.')
+            os.environ['QT'] = '6.11.1'
     return True
